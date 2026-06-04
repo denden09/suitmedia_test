5f1e3fffa0f89890a7653c0395db8ebf1a87844b
Vinsensius Arden <vinsensius.arden@gmail.com>
2026-06-04 20:54:36 +0700

    minor user

--- lib/providers/user_provider.dart
+++ lib/providers/user_provider.dart
@@ -3,38 +3,42 @@ import 'package:flutter/material.dart';
 import '../models/user_model.dart';
 import '../services/api_service.dart';
 
-class UserProvider
-    extends ChangeNotifier {
-  final ApiService _apiService =
-      ApiService();
+class UserProvider extends ChangeNotifier {
+  final ApiService _apiService = ApiService();
 
   List<UserModel> users = [];
+  List<UserModel> filteredUsers = [];
 
   bool isLoading = false;
+  bool isLoadingMore = false;
+  bool hasError = false;
 
   int page = 1;
-
   int totalPages = 1;
 
-  String selectedUser = "Selected User Name";
+  String selectedUser = "No User Selected";
 
   Future<void> fetchUsers() async {
     if (isLoading) return;
 
     isLoading = true;
+    hasError = false;
+
     notifyListeners();
 
     try {
       final result =
           await _apiService.getUsers(page);
 
-      users.addAll(
-          result['users']);
+      users.addAll(result['users']);
+
+      filteredUsers =
+          List.from(users);
 
       totalPages =
           result['total_pages'];
     } catch (e) {
-      debugPrint(e.toString());
+      hasError = true;
     }
 
     isLoading = false;
@@ -43,6 +47,7 @@ class UserProvider
 
   Future<void> refreshUsers() async {
     users.clear();
+    filteredUsers.clear();
 
     page = 1;
 
@@ -52,9 +57,42 @@ class UserProvider
   Future<void> loadMore() async {
     if (page >= totalPages) return;
 
+    isLoadingMore = true;
+    notifyListeners();
+
     page++;
 
-    await fetchUsers();
+    try {
+      final result =
+          await _apiService.getUsers(page);
+
+      users.addAll(result['users']);
+
+      filteredUsers =
+          List.from(users);
+    } catch (_) {}
+
+    isLoadingMore = false;
+    notifyListeners();
+  }
+
+  void searchUser(String query) {
+    if (query.isEmpty) {
+      filteredUsers =
+          List.from(users);
+    } else {
+      filteredUsers = users
+          .where(
+            (user) => user.fullName
+                .toLowerCase()
+                .contains(
+                  query.toLowerCase(),
+                ),
+          )
+          .toList();
+    }
+
+    notifyListeners();
   }
 
   void setSelectedUser(
